2743e18b1df665a2e60475cf007acdef578a1c31
Tatiana Smalii <89124356544@mail.ru>
2023-04-28 08:24:36 +0500

    defects test in Idea

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -227,7 +227,7 @@ class MainKtTest {
         val transfer = 15_100
         val previos = 0
         val result = comission(typeCard = type, transfer = transfer, previos = previos)
-        assertEquals(-1, result)
+        assertEquals(20, result)
     }
 
     @Test
