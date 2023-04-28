e07a8f2fa17c95124af02e04acb23039adb12ddd
Tatiana Smalii <89124356544@mail.ru>
2023-04-28 08:46:37 +0500

    new defect in test

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -10,7 +10,7 @@ class MainKtTest {
         val transfer = 500_000
         val previos = 0
         val result = comission(typeCard = type, transfer = transfer, previos = previos)
-        assertEquals(-1, result)
+        assertEquals(20, result)
     }
 
     @Test
